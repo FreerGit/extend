0c5a8069d5fc16cc442c11ad42ed8064255f12a2
sven <42868792+FreerGit@users.noreply.github.com>
2025-12-10 21:06:56 +0100

    Fix data race on arg pool

--- lib/logger.ml
+++ lib/logger.ml
@@ -26,19 +26,30 @@ module Make (Q : Queue_intf.S) : Logger_intf.S = struct
     Array.init (pool_max_size + 1) ~f:(fun _ -> Queue.create ())
   ;;
 
+  let locks = Array.init (pool_max_size + 1) ~f:(fun _ -> Stdlib.Mutex.create ())
+
   let[@inline] get_args_array size =
     if size <= pool_max_size
     then (
-      match Queue.dequeue pools.(size) with
-      | Some arr -> arr
-      | None -> Array.create ~len:size (Arg (TInt, 0)))
+      Stdlib.Mutex.lock locks.(size);
+      let result =
+        match Queue.dequeue pools.(size) with
+        | Some arr -> arr
+        | None -> Array.create ~len:size (Arg (TInt, 0))
+      in
+      Stdlib.Mutex.unlock locks.(size);
+      result)
     else Array.create ~len:size (Arg (TInt, 0))
   ;;
 
   let[@inline] return_args_array arr =
     let size = Array.length arr in
-    if size <= pool_max_size && Queue.length pools.(size) < 100
-    then Queue.enqueue pools.(size) arr
+    if size <= pool_max_size
+    then (
+      let lock = locks.(size) in
+      Stdlib.Mutex.lock lock;
+      if Queue.length pools.(size) < 100 then Queue.enqueue pools.(size) arr;
+      Stdlib.Mutex.unlock lock)
   ;;
 
   let[@inline] print_arg (buf : Buffer.t) (Arg (ty, v) : arg) : unit =
